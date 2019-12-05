ad82594b8af84d4111a6fcf47947851c429b9380
Chawn Christian <chawn.christian@gmail.com>
2019-12-04 20:52:36 -0500

    mvp

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,38 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import SmurfForm from './SmurfForm';
+import SmurfList from './Smurflist';
+import {connect} from 'react-redux';
+import {FetchSmurf, DeleteSmurf, AddSmurf} from "../actions/index";
 import "./App.css";
-class App extends Component {
-  render() {
+const App =(props)=>{
+
+  useEffect(()=>{
+      props.FetchSmurf()
+    },[])
+
+
+  
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <SmurfForm/>
+        {props.smurfs.map(item =>(
+          <SmurfList item= {item} key= {item.id} DeleteSmurf={props.DeleteSmurf}/>
+        ))}
+        
       </div>
     );
   }
-}
 
-export default App;
+
+const mapStateToProps = (state) =>{
+  return {
+    smurfs: state.smurfs,
+    isLoading: state.isLoading,
+    error: state.error
+  }
+}
+export default connect(mapStateToProps,{FetchSmurf, DeleteSmurf, AddSmurf} ) (App);
